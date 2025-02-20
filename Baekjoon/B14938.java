45b6a7e8d05a3d031ca01fdb431cb1b72711688b
minchae_laptop <alsco9707@gmail.com>
2025-02-20 20:13:24 +0900

    백준: 서강그라운드

--- Baekjoon/B14938.java
+++ Baekjoon/B14938.java
@@ -4,57 +4,59 @@ import java.util.*;
 public class B14938 {
     public static int n, m, r, answer;
     public static int[] arr, visited;
-    public static ArrayList<ArrayList<Rode>> adj;
+    public static ArrayList<ArrayList<Node>> adj;
 
-    public static class Rode implements Comparable<Rode> {
-        int end;
+    public static class Node implements Comparable<Node> {
+        int node;
         int len;
 
-        Rode(int end, int len) {
+        Node(int node, int len) {
+            this.node = node;
             this.len = len;
-            this.end = end;
         }
 
         @Override
-        public int compareTo(Rode o) {
-            return this.len - o.len;    // 오름 차순 정렬 
+        public int compareTo(Node o) {
+            return this.len - o.len;    // 오름 차순 정렬
         }
     }
     
     public static void dijkstra(int start) {
-        int sumVal = arr[start];
+        int sumVal = 0;
+        
+        // 최대 값으로 초기화
         visited = new int[n];
         for (int i=0; i<n; i++) {
             visited[i] = Integer.MAX_VALUE;
         }
-
-        PriorityQueue<Rode> pq = new PriorityQueue<>();
-        pq.add(new Rode(start, 0));
+        
+        // 우선 순위 큐에 시작 좌표 넣기
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
 
         while (!pq.isEmpty()) {
-            Rode node = pq.poll();
-            if (visited[node.end] < node.len | node.len > m) {
+            Node node = pq.poll();
+            if (visited[node.node] < node.len | node.len > m) {
                 continue;
             }
-            visited[node.end] = node.end;
-
-            for (Rode next : adj.get(node.end)) {
-                if (node.len + next.len <= m) {
-                    pq.add(new Rode(next.end, node.len + next.len));
-                }
-            }
-            for (int i=0; i<n; i++) {
-                if (visited[i] != Integer.MAX_VALUE) {
-                    sumVal += arr[i];
+            // 방문 표시
+            visited[node.node] = node.node;
+            
+            // 연결 된 다음 노드 탐색
+            for (Node next : adj.get(node.node)) {
+                int nextLen = node.len + next.len;
+                if (nextLen <= m & visited[next.node] >= nextLen) {
+                    pq.add(new Node(next.node, node.len + next.len));
                 }
             }
         }
-        StringBuilder sb = new StringBuilder();
+        
+        // 탐색 종료 후 획득 가능한 아이템 체크
         for (int i=0; i<n; i++) {
-            sb.append(visited[i]);
-            sb.append(", ");
+            if (visited[i] != Integer.MAX_VALUE) {
+                sumVal += arr[i];
+            }
         }
-        System.out.println(sb);
         answer = Math.max(answer, sumVal);
     }
 
@@ -65,12 +67,15 @@ public class B14938 {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         r = Integer.parseInt(st.nextToken());
-        arr = new int[n];
 
+        // 아이템 가치
+        arr = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i=0; i<n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+
+        // 인접 리스트
         adj = new ArrayList<>();
         for (int i=0; i<n; i++) {
             adj.add(new ArrayList<>());
@@ -80,10 +85,11 @@ public class B14938 {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int l = Integer.parseInt(st.nextToken());
-            adj.get(a - 1).add(new Rode(b - 1, l));
-            adj.get(b - 1).add(new Rode(a - 1, l));
+            adj.get(a - 1).add(new Node(b - 1, l));
+            adj.get(b - 1).add(new Node(a - 1, l));
         }
-
+        
+        // 모든 노드에 대해 최소 경로 탐색
         for (int i=0; i<n; i++) {
             dijkstra(i);
         }
